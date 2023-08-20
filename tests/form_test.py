df21883a08a36a54e4299a134c7172a625d45fdf
Alisher Duzmagambetov <alisher.001017@gmail.com>
2023-08-21 02:52:24 +0600

    added 2 data

--- tests/form_test.py
+++ tests/form_test.py
@@ -12,20 +12,20 @@ class TestFormPage:
         result_text = result[0] + " " + result[1]
         assert first_data == result_text
 
-    # def test_form2(self, driver):
-    #     form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
-    #     form_page.open()
-    #     first_data = "Johnny Depp johnny@gmail.com"
-    #     form_page.fill_form_and_submit(first_data)
-    #     result = form_page.get_result()
-    #     result_text = result[0] + " " + result[1]
-    #     assert first_data == result_text
-    #
-    # def test_form3(self, driver):
-    #     form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
-    #     form_page.open()
-    #     first_data = "Leonardo DiCaprio leonardo@gmail.com"
-    #     form_page.fill_form_and_submit(first_data)
-    #     result = form_page.get_result()
-    #     result_text = result[0] + " " + result[1]
-    #     assert first_data == result_text
+    def test_form2(self, driver):
+        form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
+        form_page.open()
+        first_data = "Johnny Depp johnny@gmail.com"
+        form_page.fill_form_and_submit(first_data)
+        result = form_page.get_result()
+        result_text = result[0] + " " + result[1]
+        assert first_data == result_text
+
+    def test_form3(self, driver):
+        form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
+        form_page.open()
+        first_data = "Leonardo DiCaprio leonardo@gmail.com"
+        form_page.fill_form_and_submit(first_data)
+        result = form_page.get_result()
+        result_text = result[0] + " " + result[1]
+        assert first_data == result_text
